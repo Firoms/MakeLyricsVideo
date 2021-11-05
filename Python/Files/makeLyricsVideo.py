81ceef2cbe0d55ec9933176db0dc77462aef9377
Firoms <edisonmessi@naver.com>
2021-11-05 16:39:25 +0900

    2021/11/05

--- Python/Files/makeLyricsVideo.py
+++ Python/Files/makeLyricsVideo.py
@@ -1,4 +1,8 @@
+import cv2
+import glob
+import moviepy.editor as mp
 import os
+import youtube_dl
 from alpGenerator import alpGenerator
 from PIL import Image, ImageDraw, ImageFont
 
@@ -9,28 +13,43 @@ class makeLyricsVideo:
     :param Name: 영상 이름
     '''
 
-    def __init__(self, Name: str) -> None:
-        self.videoName = Name
-        self.makeImg()
+    def __init__(self, name: str) -> None:
+        self.videoName = name
+        self.makeDir()
 
-    def makeDir(self, dirName) -> None:
-        pass
+    def makeDir(self) -> None:
+        """
+        영상 제작에 필요한 파일들을 저장하는 디렉토리 생성 함수
+        & Lyrics 폴더에 txt 파일 생성
+        """
+        try:
+            os.chdir('../../Lyrics')
+            os.mkdir(self.videoName)
+            os.chdir('../Musics')
+            os.mkdir(self.videoName)
+            os.chdir('../Images')
+            os.mkdir(self.videoName)
+            os.chdir('../Videos')
+            os.mkdir(self.videoName)
+        except:
+            print("이미 같은 제목의 영상이 있습니다.")
+        os.chdir('../Python/Files')
 
-    def getLyrics(self) -> None::
+    def getLyrics(self) -> None:
         '''
         영상에 들어갈 제목, 가수, 가사를 입력받는 함수
         '''
-        os.chdir('../../Lyrics')
-        os.mkdir(self.videoName)
+        fileCreate = open(f'../../Lyrics/{self.videoName}/lyrics.txt', 'w')
+        fileCreate.close()
         print(f"Lyrics/{self.videoName} 폴더 안에 생성된 lyrics.txt 파일에 가사를 입력해주세요.")
         print('첫번째 줄은 "제목 - 가수" 형식이어야 하며, 두번째 줄은 공백, 세번째 줄부터는 가사를 넣어주세요.')
         lyricsInput = input("저장 이후 파일을 닫고 엔터를 눌러주시면 진행됩니다.")
-
-    def makeTitleImg(self) -> None::
-        lyricsFile = open(
+        self.lyricsFile = open(
             f'../../Lyrics/{self.videoName}/lyrics.txt', 'r', encoding='UTF8')
-        titleLine = lyricsFile.readline()
-        title, singer = titleLine.split("-")
+
+    def makeTitleImg(self) -> None:
+        self.titleLine = self.lyricsFile.readline()
+        title, singer = self.titleLine.split("-")
         fontsFolder = '../../Fonts'
         targetImage = Image.open('../../Images/background/background.jfif')
         draw = ImageDraw.Draw(targetImage)
@@ -47,22 +66,16 @@ class makeLyricsVideo:
         draw.text((1000, 550), text=f"{singer}",
                   fill="#575759", font=selectedFont, align='center')
         targetImage.save(f"../../Images/{self.videoName}/aa.jpg")
-        blankLine = lyricsFile.readline()
-
-        curLine1 = lyricsFile.readline()
-        curLine2 = lyricsFile.readline()
-        nextLine = lyricsFile.readline()
-        alps = list(alpGenerator())
-
+        blankLine = self.lyricsFile.readline()
 
     def makeImg(self) -> None:
         '''
         영상에 들어갈 가사가 적힌 사진을 만드는 함수
         '''
-        lyricsFile = open(
-            f'../../Lyrics/{self.videoName}/lyrics.txt', 'r', encoding='UTF8')
-        titleLine = lyricsFile.readline()
-        title, singer = titleLine.split("-")
+        curLine1 = self.lyricsFile.readline()
+        curLine2 = self.lyricsFile.readline()
+        nextLine = self.lyricsFile.readline()
+        alps = list(alpGenerator())
         fontsFolder = '../../Fonts'
         idx = 0
         while curLine1:
@@ -71,7 +84,7 @@ class makeLyricsVideo:
             selectedFont = ImageFont.truetype(
                 os.path.join(fontsFolder, 'GodoB.ttf'), 30)
             draw.text(
-                (20, 20), text=f"{titleLine}", fill="#5d7530", font=selectedFont, align='center')
+                (20, 20), text=f"{self.titleLine}", fill="#5d7530", font=selectedFont, align='center')
             selectedFont = ImageFont.truetype(
                 os.path.join(fontsFolder, 'GodoM.ttf'), 40)
             draw.text((1240, 20), text=f"Lyrics WFS", fill="#123152",
@@ -86,32 +99,88 @@ class makeLyricsVideo:
                 (100, 780), text=f"{nextLine}", fill="#575759", font=selectedFont, align='center')
             targetImage.save(f"../../Images/{self.videoName}/{alps[idx]}.jpg")
             curLine1 = nextLine
-            curLine2 = lyricsFile.readline()
-            nextLine = lyricsFile.readline()
+            curLine2 = self.lyricsFile.readline()
+            nextLine = self.lyricsFile.readline()
             idx += 1
 
         targetImage = Image.open('../../Images/background/background.jfif')
         draw = ImageDraw.Draw(targetImage)
         targetImage.save(f"../../Images/{self.videoName}/{alps[idx]}.jpg")
+        self.lyricsFile.close()
 
-        lyricsFile.close()
+    def getChangeTime(self) -> None:
+        '''
+        가사를 넘겨줄 타이밍을 입력받는 함수
+        '''
+        timeList = [0]
+        print("가사를 넘길 시간을 입력해주세요\n")
+        while True:
+            time = input()
+            try:
+                timeList.append(int(time[0])*60 + int(time[1:3]))
+            except:
+                break
+
+        self.frameList = []
+        for i in range(len(timeList)):
+            if i == 0:
+                continue
+            self.frameList.append(timeList[i]-timeList[i-1])
+
 
     def makeVideo(self) -> None:
         '''
         가사가 적힌 사진을 모아 영상으로 제작하는 함수
         '''
+        frameSize = (1470, 980)
+        out = cv2.VideoWriter(f'../../Videos/{self.videoName}/{self.videoName}-nosound.avi',
+                              cv2.VideoWriter_fourcc(*'DIVX'), 1, frameSize)
+        idx = 0
+        for filename in glob.glob(f'../../Images/{self.videoName}/*.jpg'):
+            for i in range(self.frameList[idx]):
+                img = cv2.imread(filename)
+                out.write(img)
+            idx += 1
+        out.release()
 
     def downloadMusic(self) -> None:
         '''
         영상에 필요한 노래를 유튜브 링크로부터 다운로드 받는 함수
         '''
+        downloadList = []
+        youtubeURL = input("유튜브 링크를 입력해주세요 : ")
+        downloadList.append(youtubeURL)
+        output_dir = os.path.join(
+            f'../../Musics/{self.videoName}/', f'{self.videoName}.mp3')
+
+        
+
+        ydlOpt = {
+            'outtmpl': output_dir,
+            'format': 'bestaudio/best',
+        }
+
+        with youtube_dl.YoutubeDL(ydlOpt) as ydl:
+            ydl.download(downloadList)
 
     def putMusicInVideo(self) -> None:
         '''
         영상에 다운로드 받은 음악을 합쳐 완성하는 함수
         '''
+        audio = mp.AudioFileClip(f"../../Musics/{self.videoName}/{self.videoName}.mp3")
+        video1 = mp.VideoFileClip(f"../../Videos/{self.videoName}/{self.videoName}-nosound.avi")
+        final = video1.set_audio(audio)
+        final.write_videofile(
+            f"../../Videos/{self.videoName}/{self.videoName}.mp4", codec='mpeg4', audio_codec='libvorbis')
 
 
 if __name__ == '__main__':
-    Name = input()
-    Video = makeLyricsVideo(Name)
+    name = input("영상 제목을 영어로 입력해주세요 : ")
+    video = makeLyricsVideo(name)
+    video.getLyrics()
+    video.makeTitleImg()
+    video.makeImg()
+    video.getChangeTime()
+    video.makeVideo()
+    video.downloadMusic()
+    video.putMusicInVideo()
