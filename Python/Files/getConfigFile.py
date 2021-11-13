587c6e25756279047615db8cccf89d7ba0489778
Firoms <edisonmessi@naver.com>
2021-11-13 10:29:26 +0900

    2021/11/13

---
+++ Python/Files/getConfigFile.py
@@ -0,0 +1,11 @@
+import configparser
+
+class ConfigManager():
+    def __init__(self) -> None:
+        self.config = configparser.ConfigParser()
+        
+    def makeConfigFile(self) -> None:
+        pass
+
+    def readConfigFile(self) -> None:
+        pass
