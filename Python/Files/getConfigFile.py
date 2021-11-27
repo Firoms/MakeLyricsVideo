f85ecc32dca73da8c8e23288a4b07d2f057c7932
Firoms <edisonmessi@naver.com>
2021-11-27 14:09:55 +0900

    2021/11/27

--- Python/Files/getConfigFile.py
+++ Python/Files/getConfigFile.py
@@ -9,7 +9,7 @@ class ConfigManager():
     def makeDefaultConfigFile(self) -> None:
         self.config['System'] = {}
         self.config['System']['Version'] = '0.8'
-        self.config['System']['update'] = strftime('%Y-%m-%d %H:%M:%S')
+        self.config['System']['Update'] = strftime('%Y-%m-%d %H:%M:%S')
         self.config['System']['Maker'] = 'Lyrics WFS'
 
         defaultWritings = {
@@ -32,54 +32,13 @@ class ConfigManager():
             self.config[f'{key}']['PosY'] = val[3]
             self.config[f'{key}']['Color'] = val[4]
 
-        with open('config.ini', 'w', encoding='cp949') as configfile:
+        with open('../../Settings/config.ini', 'w', encoding='cp949') as configfile:
             self.config.write(configfile)
 
     def readConfigFile(self) -> None:
-        pass
+        self.config.read('../../Settings/config.ini', encoding='cp949')
 
 
 if __name__ == "__main__":
     CM = ConfigManager()
     CM.makeDefaultConfigFile()
-
-'''
-import configparser
-from time import strftime
-
-
-def config_generator():
-    # 설정파일 만들기
-    config = configparser.ConfigParser()
-
-    # 설정파일 오브젝트 만들기
-    config['system'] = {}
-    config['system']['title'] = 'Neural Networks'
-    config['system']['version'] = '1.2.42'
-    config['system']['update'] = strftime('%Y-%m-%d %H:%M:%S')
-
-    config['video'] = {}
-    config['video']['width'] = '640'
-    config['video']['height'] = '480'
-    config['video']['type'] = 'avi'
-
-    # 설정파일 저장
-    with open('config.ini', 'w', encoding='utf-8') as configfile:
-        config.write(configfile)
-
-
-def config_read():
-    
-    # 설정파일 읽기
-    config = configparser.ConfigParser()    
-    config.read('config.ini', encoding='utf-8') 
-
-    # 설정파일의 색션 확인
-    # config.sections())
-    ver = config['system']['version']
-    title = config['system']['title']
-    print(title, ver)
-
-config_generator()
-config_read()
-'''
